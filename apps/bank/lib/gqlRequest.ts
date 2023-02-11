d313aa265d8700dfd914f69e3e0c0409bbb12a02
sunginHwang <gommpo111@gmail.com>
2023-02-11 17:12:54 +0900

    reset product

--- apps/bank/lib/gqlRequest.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-
-const endpoint = 'http://localhost:3333/graphql';
-const gqlRequest = new GraphQLClient(endpoint);
-
-export default gqlRequest;
--- libs/ui/wds/src/lib/base/Button/index.tsx
+++
@@ -1,6 +0,0 @@
-
-const Button = () => {
-  return <button>버튼 공통</button>
-}
-
-export default Button;
