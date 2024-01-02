7fcc3082f976e7bae4808abd2e942850546b914b
sunginHwang <gommpo111@gmail.com>
2024-01-02 19:30:41 +0900

    feat: 남은날짜 구하는 포맷 버그 수정

--- apps/woolbank/utils/date.ts
+++ apps/woolbank/utils/date.ts
@@ -1,14 +1,15 @@
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 export const getRemainDay = (date: string | number, { completeMsg = 'd-day' }: { completeMsg: string }) => {
-  const now = dayjs();
-  const compareDay = dayjs(date);
+  const resetHours = (date: Dayjs) => date.hour(0).minute(0).second(0).millisecond(0);
+
+  const now = resetHours(dayjs());
+  const compareDay = resetHours(dayjs(date));
 
   if (now.get('M') === compareDay.get('M') && now.get('d') === compareDay.get('d')) {
     return { remainDay: 0, remainDayKo: completeMsg };
   }
 
-  const remainDay = now.diff(compareDay, 'D');
-
-  return { remainDay, remainDayKo: remainDay > 7 ? compareDay.format('MM월 D') : `${remainDay}일 뒤` };
+  const remainDay = Math.abs(now.diff(compareDay, 'day'));
+  return { remainDay, remainDayKo: remainDay > 7 ? compareDay.format('MM월 D일') : `${remainDay}일 뒤` };
 };
