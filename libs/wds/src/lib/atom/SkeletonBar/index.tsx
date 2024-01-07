5ae460ca69bc496900ca97a95b8d43a4a59ec38b
sunginHwang <gommpo111@gmail.com>
2024-01-07 15:15:34 +0900

    feat: 스켈레톤바 props 확장 및 기본 마진 제거

--- libs/wds/src/lib/atom/SkeletonBar/index.tsx
+++ libs/wds/src/lib/atom/SkeletonBar/index.tsx
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
+import { FC, HTMLAttributes } from 'react';
 import animations from '../../style/animations';
-import { FC } from 'react';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   width?: string;
   height?: string;
   radius?: number;
 }
 
-export const SkeletonBar: FC<Props> = ({ width = '20rem', height = '3.2rem', radius = 3 }) => {
-  return <SC.Contaienr width={width} height={height} radius={radius} />;
+export const SkeletonBar: FC<Props> = ({ width = '20rem', height = '3.2rem', radius = 3, ...rest }) => {
+  return <SC.Contaienr width={width} height={height} radius={radius} {...rest} />;
 };
 
 const SC = {
@@ -18,6 +18,5 @@ const SC = {
     height: ${({ height }) => height};
     animation: ${animations.loading} 1.3s infinite ease-in-out;
     border-radius: ${({ radius }) => radius}px;
-    margin-bottom: 0.5rem;
   `,
 };
