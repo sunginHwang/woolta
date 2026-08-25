aab9ac0eb679e09dbd51d75fc6fb27a10d0cc3d8
sunginHwang <gommpo111@gmail.com>
2026-08-25 14:37:56 +0900

    feat: 라우트 전환 로딩 스켈레톤 추가 (RSC 대기 체감 개선)

---
+++ apps/woolta/app/bank/loading.tsx
@@ -0,0 +1,5 @@
+import { PageSkeleton } from '../../components/loading/PageSkeleton';
+
+export default function BankLoading() {
+  return <PageSkeleton />;
+}
---
+++ apps/woolta/components/loading/PageSkeleton.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { SkeletonBar } from '@wds';
+import { styled } from 'styled-components';
+
+/**
+ * 라우트 전환(RSC 로딩) 동안 콘텐츠 영역에 표시하는 공용 스켈레톤.
+ * 각 앱 세그먼트의 loading.tsx 에서 사용한다.
+ */
+export const PageSkeleton = () => {
+  return (
+    <SC.Container>
+      <SkeletonBar width='18rem' height='2.8rem' radius={8} />
+      <SC.Rows>
+        <SkeletonBar width='100%' height='4.4rem' radius={10} />
+        <SkeletonBar width='100%' height='4.4rem' radius={10} />
+        <SkeletonBar width='82%' height='4.4rem' radius={10} />
+        <SkeletonBar width='90%' height='4.4rem' radius={10} />
+        <SkeletonBar width='70%' height='4.4rem' radius={10} />
+      </SC.Rows>
+    </SC.Container>
+  );
+};
+
+const SC = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    max-width: 72rem;
+    padding: 2.4rem;
+  `,
+  Rows: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+  `,
+};
